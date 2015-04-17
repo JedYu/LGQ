28c6be7bcd04cb7c475b14896e0b0ffeef20396c
YuJian <yujian@airimos.com>
2015-04-17 10:56:12 +0800

    change to BaiduMap

--- app/src/main/java/li/vane/ex/lgq/MainActivity.java
+++ app/src/main/java/li/vane/ex/lgq/MainActivity.java
@@ -1,25 +1,30 @@
 package li.vane.ex.lgq;
 
 import android.content.res.AssetManager;
-import android.graphics.Color;
-import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.activeandroid.query.Select;
-import com.amap.api.location.AMapLocation;
-import com.amap.api.location.AMapLocationListener;
-import com.amap.api.location.LocationManagerProxy;
-import com.amap.api.location.LocationProviderProxy;
-import com.amap.api.maps2d.AMap;
-import com.amap.api.maps2d.LocationSource;
-import com.amap.api.maps2d.MapView;
-import com.amap.api.maps2d.model.LatLng;
-import com.amap.api.maps2d.model.Polygon;
-import com.amap.api.maps2d.model.PolygonOptions;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.map.Polygon;
+import com.baidu.mapapi.map.PolygonOptions;
+import com.baidu.mapapi.map.Stroke;
+import com.baidu.mapapi.model.LatLng;
 
 import org.jamel.dbf.processor.DbfProcessor;
 import org.jamel.dbf.processor.DbfRowMapper;
@@ -37,153 +42,193 @@ import li.vane.ex.lgq.bean.LGQ;
 import li.vane.ex.lgq.bean.PolygonPoint;
 
 
-public class MainActivity extends ActionBarActivity implements LocationSource, AMapLocationListener, AMap.OnMapClickListener
+public class MainActivity extends ActionBarActivity implements BDLocationListener
 {
     private static final String TAG = MainActivity.class.getSimpleName();
-    private AMap mAMap;
-    private MapView mMapView;
-    private LocationSource.OnLocationChangedListener mListener;
-
-    private LocationManagerProxy mLocationManagerProxy;
+    private MapView mMapView = null;
+    private BaiduMap mBaiduMap = null;
+    public LocationClient mLocationClient = null;
+    private Handler mUIHandler = null;
 
     private List<Polygon> mLgqPolygons = new ArrayList<Polygon>();
+
+    private static final LatLng CENTER = new LatLng(29.912988, 121.478965);
+
+
+    private BaiduMap.OnMapStatusChangeListener mMapStatusChangeListener = new BaiduMap.OnMapStatusChangeListener()
+    {
+
+        @Override
+        public void onMapStatusChangeStart(MapStatus arg0)
+        {
+        }
+
+        @Override
+        public void onMapStatusChangeFinish(MapStatus arg0)
+        {
+        }
+
+        @Override
+        public void onMapStatusChange(MapStatus arg0)
+        {
+        }
+    };
+
+
+    private BDLocationListener mLocationListener = new BDLocationListener()
+    {
+
+        @Override
+        public void onReceiveLocation(BDLocation location)
+        {
+            if (location.getLocType() == BDLocation.TypeGpsLocation
+                    || location.getLocType() == BDLocation.TypeNetWorkLocation
+                    || location.getLocType() == BDLocation.TypeOffLineLocation)
+            {
+                Log.d(TAG, "getLatitude:" + location.getLatitude() + ", getLongitude:" + location.getLongitude());
+
+                MyLocationData locData = new MyLocationData.Builder().accuracy(location.getRadius())
+                        // 此处设置开发者获取到的方向信息，顺时针0-360
+                        .direction(100).latitude(location.getLatitude()).longitude(location.getLongitude()).build();
+
+                mBaiduMap.setMyLocationData(locData);
+
+
+//                LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+//                MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(ll);
+//                mBaiduMap.animateMapStatus(u);
+            }
+            else
+            {
+                Log.w(TAG, "getLocType:" + location.getLocType());
+            }
+        }
+    };
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+
         setContentView(R.layout.activity_main);
+
+        mUIHandler = new Handler(getMainLooper());
+        Log.d(TAG, "------onCreate------");
         loadDBF();
         mMapView = (MapView) findViewById(R.id.map);
-        mMapView.onCreate(savedInstanceState);
+        mBaiduMap = mMapView.getMap();
 
-        mAMap = mMapView.getMap();
+        initMap();
 
-        mAMap.setLocationSource(this);
-        mAMap.getUiSettings().setMyLocationButtonEnabled(true);
-        mAMap.setMyLocationEnabled(true);
+        Log.d(TAG, "------ onCreate end------");
+    }
 
-        mAMap.setOnMyLocationChangeListener(new AMap.OnMyLocationChangeListener()
+
+    private void initMap()
+    {
+        mMapView.showZoomControls(false);
+        mMapView.setKeepScreenOn(true);
+        mBaiduMap.setMyLocationEnabled(true);
+        // 设置中心点
+        mBaiduMap.setMapStatus(MapStatusUpdateFactory.newLatLng(new LatLng(30.419, 120.300)));
+
+        // 设置默认显示级别
+        mBaiduMap.setMapStatus(MapStatusUpdateFactory.zoomTo(17));
+
+        mBaiduMap.setOnMapStatusChangeListener(mMapStatusChangeListener);
+
+        // 定位初始化
+        mLocationClient = new LocationClient(this);
+        mLocationClient.registerLocationListener(mLocationListener);
+
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Device_Sensors);// 设置定位模式
+        option.setCoorType("bd09ll");// 返回的定位结果是百度经纬度,默认值gcj02
+        option.setScanSpan(60000);// 设置发起定位请求的间隔时间为5000ms
+        option.setIsNeedAddress(true);// 返回的定位结果包含地址信息
+        option.setNeedDeviceDirect(true);// 返回的定位结果包含手机机头的方向
+        mLocationClient.setLocOption(option);
+        mLocationClient.start();
+
+        MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(CENTER);
+        mBaiduMap.animateMapStatus(u);
+
+
+
+        mBaiduMap.setOnMapLoadedCallback(new BaiduMap.OnMapLoadedCallback()
         {
             @Override
-            public void onMyLocationChange(Location location)
+            public void onMapLoaded()
             {
-                Log.d(TAG, "onMyLocationChange:" + location);
+
+                Log.d(TAG, "------ onMapLoaded------");
+                new Thread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        addLgqPolygons();
+                    }
+                }).start();
+                Log.d(TAG, "------ onMapLoaded end------");
+
+
             }
         });
+    }
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+    }
 
+    @Override
+    public void onReceiveLocation(BDLocation location)
+    {
+        if (location == null)
+            return ;
+
+        Log.d(TAG, "onReceiveLocation:" + location);
+
+    }
+
+    private void addLgqPolygons()
+    {
         List<LGQ> lgqs = getAll();
         Log.d(TAG, "city:" + lgqs.get(0).city);
+        Log.d(TAG, "lat:" + lgqs.get(0).polygon().get(0).lat);
+        Log.d(TAG, "lng:" + lgqs.get(0).polygon().get(0).lng);
         Log.d(TAG, "all:" + lgqs);
 
 
         for (LGQ lgq:lgqs)
         {
-            PolygonOptions options = new PolygonOptions();
-
+            List<LatLng> pts = new ArrayList<LatLng>();
             List<PolygonPoint> points = lgq.polygon();
             int numPoints = points.size();
-            // 绘制一个椭圆
-            for (int i = 0; i <= numPoints; i++)
+
+            for (int i = 0; i < numPoints; i=i+5)
             {
-                int index = i;
-                if (index == numPoints)
-                {
-                    index = 0;
-                }
-                options.add(new LatLng(points.get(index).lat, points.get(index).lng));
+                pts.add(new LatLng(points.get(i).lat, points.get(i).lng));
             }
 
-            Polygon polygon = mAMap.addPolygon(options.strokeWidth(1f)
-                    .strokeColor(Color.parseColor("#FFCCCCCC")).fillColor(Color.parseColor("#9900FF00")));
+            pts.add(new LatLng(points.get(0).lat, points.get(0).lng));
 
-            mLgqPolygons.add(polygon);
+            OverlayOptions polygonOption = new PolygonOptions()
+                    .points(pts)
+                    .stroke(new Stroke(5, 0xAA00FF00))
+                    .fillColor(0xAAFFFF00);
+
+            mBaiduMap.addOverlay(polygonOption);
+
         }
 
-        mAMap.setOnMapClickListener(this);
-
     }
 
-    @Override
-    public void onMapClick(LatLng point)
-    {
-        for (Polygon p :mLgqPolygons)
-        {
-            if (p.contains(point))
-            {
-                p.setFillColor(Color.parseColor("#99FFFF00"));
-
-                mAMap.postInvalidate();
-            }
-        }
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-
-    /**
-     * 定位成功后回调函数
-     */
-    @Override
-    public void onLocationChanged(AMapLocation aLocation) {
-        if (mListener != null) {
-            mListener.onLocationChanged(aLocation);// 显示系统小蓝点
-        }
-    }
-
-    /**
-     * 激活定位
-     */
-    @Override
-    public void activate(LocationSource.OnLocationChangedListener listener)
-    {
-        mListener = listener;
-        if (mLocationManagerProxy == null)
-        {
-            mLocationManagerProxy = LocationManagerProxy.getInstance(this);
-            /*
-			 * mAMapLocManager.setGpsEnable(false);//
-			 * 1.0.2版本新增方法，设置true表示混合定位中包含gps定位，false表示纯网络定位，默认是true
-			 */
-            // Location API定位采用GPS和网络混合定位方式，时间最短是2000毫秒
-            mLocationManagerProxy.requestLocationData(
-                    LocationProviderProxy.AMapNetwork, 2000, 10, this);
-        }
-    }
-
-    /**
-     * 停止定位
-     */
-    @Override
-    public void deactivate()
-    {
-        mListener = null;
-        if (mLocationManagerProxy != null)
-        {
-            mLocationManagerProxy.removeUpdates(this);
-            mLocationManagerProxy.destroy();
-        }
-        mLocationManagerProxy = null;
-
-    }
 
     public static List<LGQ> getAll() {
         return new Select()
@@ -324,23 +369,15 @@ public class MainActivity extends ActionBarActivity implements LocationSource, A
         mMapView.onPause();
     }
 
-    /**
-     * 方法必须重写
-     */
-    @Override
-    protected void onSaveInstanceState(Bundle outState)
-    {
-        super.onSaveInstanceState(outState);
-        mMapView.onSaveInstanceState(outState);
-    }
 
-    /**
-     * 方法必须重写
-     */
     @Override
     protected void onDestroy()
     {
         super.onDestroy();
         mMapView.onDestroy();
+
+        mLocationClient.unRegisterLocationListener(mLocationListener);
+        mLocationClient.stop();
+
     }
 }
